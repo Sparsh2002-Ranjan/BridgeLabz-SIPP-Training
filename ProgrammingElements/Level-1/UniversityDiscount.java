646ed499ce636ba29694677f753148e17904613a
Sparsh Ranjan <ranjansparsh872@gmail.com>
2025-06-24 08:54:46 +0530

    sparsh completed the class and object assignment

--- ProgrammingElements/Level-1/UniversityDiscount.java
+++
@@ -1,8 +0,0 @@
-public class UniversityDiscount{
-    public static void main(String[] args) {
-        int fee=125000;
-        double discount=(fee*10)/100;
-        double newfee=fee-discount;
-        System.out.print("The discount amount is INR "+ discount +"and final discounted fee is INR" + newfee);
-    }
-}
