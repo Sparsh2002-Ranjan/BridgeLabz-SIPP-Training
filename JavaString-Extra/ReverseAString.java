e395298d01b0b056b062232de971f4884f8bf1c3
Sparsh Ranjan <ranjansparsh872@gmail.com>
2025-06-26 08:26:13 +0530

    sparsh ranjan compled the assignment on strings

---
+++ JavaString-Extra/ReverseAString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ReverseAString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String st=sc.next();
+        char[] ch=st.toCharArray();
+        for(int i=ch.length-1;i>=0;i--){
+            System.out.print(ch[i]);
+        }
+    }
+}
