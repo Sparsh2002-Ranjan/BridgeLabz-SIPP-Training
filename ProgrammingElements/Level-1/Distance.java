96377921632c775043109f692d9229e8096e61cb
Sparsh Ranjan <ranjansparsh872@gmail.com>
2025-07-14 11:41:31 +0530

    deleted folder

--- ProgrammingElements/Level-1/Distance.java
+++
@@ -1,8 +0,0 @@
-public class Distance {
-    public static void main(String[] args) {
-        double kilo=10.8;
-        double miles=1.6;
-        double a=kilo*miles;
-        System.out.print("The distance 10.8 km in miles is" + a);
-    }
-}
--- ProgrammingElements/Level-1/EarthVolume.java
+++
@@ -1,8 +0,0 @@
-public class EarthVolume {
-    public static void main(String[] args) {
-        int r=6378;
-        double vol=(4/3)*3.14*r*r*r;
-        double miles=vol*1.6;
-        System.out.print("The volume of earth in cubic kilometers is " + vol + " and cubic miles is" + miles);
-    }
-}
